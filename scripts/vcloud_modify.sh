fd8eb0d5da86b6785df6b17cb93bebbd2438be2c
zjukdk <496224234@qq.com>
2023-02-02 13:38:09 -0800

    readme updated

--- scripts/vcloud_modify.sh
+++ scripts/vcloud_modify.sh
@@ -2,25 +2,25 @@
 #
 USERNAME=ubuntu
 HOSTS="$1"
-#IDENTITY="~/kdkCA.pem"
 IDENTITY="~/kdk_oracle.key"
 
 for HOSTNAME in ${HOSTS}; do
-#	ssh -i ${IDENTITY} -n -o BatchMode=yes -o StrictHostKeyChecking=no -l ${USERNAME} ${HOSTNAME} "
-#  echo '* soft nofile 50000' | sudo tee --append /etc/security/limits.conf
-#  "
+
 	ssh -i ${IDENTITY} -n -o BatchMode=yes -o StrictHostKeyChecking=no -l ${USERNAME} ${HOSTNAME} "
   rm -rf resdb
   mkdir resdb
   cd resdb
   mkdir results
+  sudo apt-get update
+  sudo apt-get upgrade
+  sudo apt-get install gcc -y
+  sudo apt-get install g++ -y
+  sudo apt-get install cmake -y
   "
-#  echo '* hard nofile 90000' | sudo tee --append /etc/security/limits.conf
-#  "
-done
 
-  # sudo apt-get update
-  # sudo apt-get upgrade
-  # sudo apt-get install gcc -y
-  # sudo apt-get install g++ -y
-  # sudo apt-get install cmake -y
+  sudo sed -i 's/#MaxSessions 10/MaxSessions 200/g' /etc/ssh/sshd_config &
+  ssh -i ${IDENTITY} -n -o BatchMode=yes -o StrictHostKeyChecking=no -l ${USERNAME} ${HOSTNAME} "
+  sudo iptables-restore < /etc/iptables/rules.v4
+  "
+
+done
